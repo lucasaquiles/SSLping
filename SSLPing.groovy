caed492fbed9828c909f2dad10bb9475436dc178
Lucas Aquiles <lucasaquiles@gmail.com>
2020-03-04 18:10:31 -0300

    refatoração

--- SSLPing.groovy
+++ SSLPing.groovy
@@ -1,27 +1,24 @@
 @Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7')
 @Grab(group = 'org.apache.httpcomponents', module = 'httpclient', version = '4.5.11')
-import groovyx.net.http.HTTPBuilder
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
-@Grab(group='org.apache.httpcomponents', module='httpclient', version='4.5.11')
-
-
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7')
+@Grab(group = 'org.apache.httpcomponents', module = 'httpclient', version = '4.5.11')
 import groovyx.net.http.HTTPBuilder
 
 import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLSocket
 import java.security.SecureRandom
 
-import static groovyx.net.http.Method.HEAD
+import static groovyx.net.http.Method.GET
 
-def requestURL = "<HOST>"
+def requestURL = ""
 def http = new HTTPBuilder( requestURL)
 
-def sslProtocols = ['TLSv1', 'TLSv1.1', 'TLSv1.2']
+def sslProtocols = ['TLSv1.2']
 
 println "iniciando script pra url ${requestURL}"
 
-
 def sslContext = SSLContext.getInstance("TLS")
+println "sslContext ${sslContext}"
 sslContext.init(null, null, new SecureRandom())
 
 def sf = new org.apache.http.conn.ssl.SSLSocketFactory(sslContext) {
@@ -29,7 +26,7 @@ def sf = new org.apache.http.conn.ssl.SSLSocketFactory(sslContext) {
     protected void prepareSocket(final SSLSocket socket) throws IOException {
         if (sslProtocols) {
 
-            socket.setEnabledProtocols(['TLSv1', 'TLSv1.1', 'TLSv1.2'] as String[])
+            socket.setEnabledProtocols(sslProtocols as String[])
         }
     }
 }
@@ -38,8 +35,11 @@ http.client.connectionManager.schemeRegistry.register(
         new org.apache.http.conn.scheme.Scheme("https", sf, 443)
 )
 
-def status = http.request( HEAD ) {
+def status = http.request( GET ) {
 
-    println "status ---"
-    response.success = { it.status }
+    response.success = { res, reader ->
+	println "reader: ${reader}"
+        println "response: ${res}" 
+    }
+//    println "status ${statuis}"
 }
